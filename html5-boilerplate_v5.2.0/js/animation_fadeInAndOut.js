ccea0fc5ab73158afd509d724636a22b657f67de
Pedro Armenta Cota <pedrohcota7@gmail.com>
2015-09-16 18:08:47 -0500

    uploaded rubberBand.js and fadeInAndOut.js and edited index.html

---
+++ html5-boilerplate_v5.2.0/js/animation_fadeInAndOut.js
@@ -0,0 +1,20 @@
+
+      $( "#nameId" ).mouseover(function() {
+      $( this ).fadeOut( 100 );
+      $( this ).fadeIn( 500 );
+    });
+
+      $( "#contactMe" ).mouseover(function() {
+      $( this ).fadeOut( 100 );
+      $( this ).fadeIn( 500 );
+    });
+
+      $( "#aboutId" ).mouseover(function() {
+      $( this ).fadeOut( 100 );
+      $( this ).fadeIn( 500 );
+    });
+
+      $( "#portfolioId" ).mouseover(function() {
+      $( this ).fadeOut( 100 );
+      $( this ).fadeIn( 500 );
+    });
